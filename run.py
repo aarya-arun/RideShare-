c7837df2193115d640771c26da1bd192c6cf318a
Aarya Arun <45157845+aarya-arun@users.noreply.github.com>
2020-02-22 11:22:46 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,48 +1,28 @@
 import flask
-from flask import request, jsonify
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_mysqldb import MySQL
+app = Flask(__name__)
 #from flask_api import status
 import datetime
 
 
-app = flask.Flask(__name__)
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] =  'indu'
+app.config['MYSQL_PASSWORD'] = 'indu123'
+app.config['MYSQL_DB'] = 'test'
+
+mysql = MySQL(app)
+
 app.config["DEBUG"] = True
 
 # Create some test data for our catalog in the form of a list of dictionaries.
 
-rides = [
-    {'rideId': 10000,
-     'created_by': "indu_r",
-     'users': ["indu_r", "aarya_a"],
-     'timestamp': "02-02-2020:19:05:66",
-     'source': 1,
-      'destination': 2}
-]
-
-
-
-users=[
-    {
-        'username':"indu_r",
-        'password': "bullshit"
-    },
-
-    {
-        'username': "aarya_a",
-        'password': "bullfuck"
-    },
-
-    {
-        'username': "maneesha_s",
-        'password': "juice_kudithiya"
-    }
-]
 
 rideidstart=10000
+p=0
+argg="LOL"
+argnum=0
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
 @app.route('/api/v1/users/all', methods=['GET'])
@@ -56,14 +36,10 @@ def api_allrides():
 
 
 # ADD A NEW USER, API=1
-@app.route('/api/v1/users', methods=['PUT'])
+@app.route('/api/v1/users', methods=['PUT', 'POST'])
 def api_addnewuser():
-
-
-
-    for user in users:
-        if user['username'] == request.json.get('username'):
-            return "This user already exists!",400
+    if flask.request.method == 'POST':
+        return "Method not allowed!",405
     
     if(len(request.json.get('password'))!=40):
         return "Bad password! Please enter a new one.",400
@@ -71,33 +47,26 @@ def api_addnewuser():
     for letter in request.json.get('password'):
         if(letter not in {'a','b','c','d','e','f','A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'}):
             return "Bad password! Please enter a new one.",400
-   
-    newuser={
-
-        'username': request.json.get('username'),
-        'password': request.json.get('password')
-    }
-
-    results=[]
-    users.append(newuser)
-
-    return jsonify(results),201
+    
+    
+    global p
+    p=1
+    return redirect(flask.url_for('readfromdb'), code=307)
 
 
 
 
 # DELETE A USER, API=2
-@app.route('/api/v1/users/<usn>', methods=['DELETE'])
+@app.route('/api/v1/users/<usn>', methods=['DELETE', 'POST'])
 def api_deluser(usn):
-
-    usn1 = usn
-    results = []
-    for user in users:
-        if user['username'] == usn1:
-            users.remove(user)
-            return jsonify(results),200
-
-    return "User never existed. Can't delete.",400
+    if flask.request.method == 'POST':
+        return "Method not allowed!",405
+    global p
+    p=2
+    global argg
+    argg=usn
+    print(type(argg))
+    return redirect(flask.url_for('readfromdb'), code=307)
 
 
 
@@ -106,25 +75,9 @@ def api_deluser(usn):
 @app.route('/api/v1/rides', methods=['POST'])
 def api_addnewride():
    
-
-    for user in users:
-        if user['username'] == request.json.get('created_by'):
-            global rideidstart
-            rideidstart=rideidstart+1
-            newride={
-
-                'created_by': request.json.get('created_by'),
-                'timestamp': request.json.get('timestamp'),
-                'source': request.json.get('source'),
-                'destination': request.json.get('destination'),
-                'users': [request.json.get('created_by')],
-                'rideId': rideidstart
-            }
-            results=[]
-            rides.append(newride)
-            return jsonify(results),200
-
-    return "This user doesn't exist! Join now to create a ride.",400
+    global p
+    p=3
+    return redirect(flask.url_for('readfromdb'), code=307)
 
 
 
@@ -138,28 +91,16 @@ def api_sourceanddest():
 
     if 'destination' not in request.args:
         return "Error! Please provide a destination.",400
+
+    global p
+    p=4
+    global a
+    a=int(request.args.get('source'))
+    global b
+    b=int(request.args.get('destination'))
+    return redirect(flask.url_for('readfromdb'), code=307)
+
     
-    source = int(request.args.get('source'))
-    destination = int(request.args.get('destination'))
-  
-    results = []
-
-
-
-    for ride in rides:
-        if ride['source'] == source and ride['destination'] == destination and ride['timestamp']>datetime.date.today().strftime("%d-%m-%Y:%S-%M-%H"):
-            newride={ 
-
-                'rideId': ride['rideId'],
-                'username': ride['created_by'],
-                'timestamp': ride['timestamp']
-            }
-            results.append(newride)
-    if results==0:
-        p=204
-    else: 
-        p=200
-    return jsonify(results),p
 
 
 
@@ -167,16 +108,15 @@ def api_sourceanddest():
 # LIST ALL DETAILS OF A GIVEN RIDE API=5
 @app.route('/api/v1/rides/<id>', methods=['GET'])
 def api_id(id):
-    id1 = int(id)
-
-    results = []
-
-    for ride in rides:
-        if ride['rideId'] == id1:
-            results.append(ride)
-            return jsonify(results),200
-
-    return "This ride doesn't exist. Sorry!",204
+    if flask.request.method == 'POST':
+        return "Method not allowed!", 405 
+    
+    global p
+    p=5
+    global argnum
+    argnum=id
+    print(type(argnum))
+    return redirect(flask.url_for('readfromdb'), code=307)
 
 
 
@@ -186,52 +126,218 @@ def api_id(id):
 @app.route('/api/v1/rides/<rideid>', methods=['POST'])
 def api_joinride(rideid):
 
-    usn= request.json.get('username')
-    id = int(rideid)
-    results=[]
+    global p
+    p=6
+    global argnum
+    argnum=rideid
+    return redirect(flask.url_for('readfromdb'), code=307)
 
-    flagu=0
-    flagr=0
 
-    for user in users:
-        if user['username']== request.json.get('username'):
-            flagu=1
 
-    for ride in rides:
-        if ride['rideId'] == id:
-            flagr=1
-
-    if flagu==0:
-        return "This user doesn't exist! Join today to create/join rides.",204
-    if flagr==0:
-        return "This ride doesn't exist. Sorry!",204
-
-    if flagu==1 and flagr==1:
-        for ride in rides:
-            if ride['rideId'] ==id:
-                if usn in ride['users']:
-                    return "This user is already part of the ride."
-                else:
-                    ride['users'].append(usn)
-    
-    return jsonify(results),200
 
 
 
 # DELETE A RIDE, API=7
-@app.route('/api/v1/rides/<rideid>', methods=['DELETE'])
+@app.route('/api/v1/rides/<rideid>', methods=['DELETE', 'POST'])
 def api_delride(rideid):
+    if flask.request.method == 'POST':
+        return "Method not allowed!", 405 
+    
+    global p
+    p=7
+    global argg
+    argg=rideid
+    return redirect(flask.url_for('readfromdb'), code=307)
+
+
+
+
+
+
+
+#WRITE TO A DB, API=8
+
+@app.route('/api/v1/db/write', methods=['POST', 'PUT', 'DELETE', 'GET'])
+def writetodb():
+    results=[]
+    global p
+    if p==1:
+        username = request.json.get('username')
+        password = request.json.get('password')
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(username, password) VALUES (%s, %s)", (username, password))
+        mysql.connection.commit()
+        cur.close()
+        results=[]
+        return jsonify(results), 201
+        
+    if p==2:
+        cur = mysql.connection.cursor()
+        j=argg
+        cur.execute("DELETE FROM users WHERE username='"+j+"'" )
+        mysql.connection.commit()
+        cur.close()
+        return jsonify(results), 200
+    if p==3:
+        created_by= request.json.get('created_by')
+        timestamp=request.json.get('timestamp')
+        source=request.json.get('source')
+        destination=request.json.get('destination')
+        global rideidstart
+        rideidstart+=1
+        rideid=rideidstart
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO rides(created_by, timestamp1, source1, destination1, rideid) VALUES (%s, %s, %s, %s, %s)", (created_by, timestamp, source, destination, rideid))
+        cur.execute("INSERT INTO ride_users(rideid,userz) VALUES (%s, %s)", (rideid,created_by))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify(results), 201
+        
+    
+    if p==6:
+        cur = mysql.connection.cursor()
+        j=argnum
+        cur.execute("INSERT INTO ride_users(rideid,userz) VALUES (%s, %s)",(j,request.json.get('username')))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify(results), 200
+        
+
+    if p==7:
+        cur = mysql.connection.cursor()
+        j=argg
+        cur.execute("DELETE FROM rides WHERE rideid='"+j+"'" )
+        mysql.connection.commit()
+        cur.close()
+        return jsonify(results), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/api/v1/db/read', methods=['POST', 'PUT', 'DELETE', 'GET'])
+def readfromdb():
+    result=[]
+    if p==1:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT *  FROM users where username='"+request.json.get('username')+"'")
+        row = cur.fetchone()
+        if(row!=None):
+            return "This user already exists!",400
+        
+        else:
+            return redirect(flask.url_for('writetodb'), code=307)
+
+    if p==2:
+        cur = mysql.connection.cursor()
+        j=argg
+        cur.execute("SELECT *  FROM users where username='"+j+"'")
+        row = cur.fetchone()
+        if(row==None):
+            return "This user doesn't exit. Can't delete.",400
+        
+        else:
+            return redirect(flask.url_for('writetodb'), code=307)
+    
+    if p==3:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT *  FROM users where username='"+request.json.get('created_by')+"'")
+        row = cur.fetchone()
+        if(row==None):
+            return "User doesn't exist. Join now to create a ride!",400
+        
+        else:
+            return redirect(flask.url_for('writetodb'), code=307)
 
     
-    results = []
-    for ride in rides:
-        if ride['rideId'] == int(rideid):
-            rides.remove(ride)
-            return jsonify(results),200
+    if p==4:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT *  FROM rides where source1=%d AND destination1=%d", (request.args.get('source'),request.args.get('destination')))
+        #datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S') 
+        row1 = cur.fetchall()
+        res=[]
+        for row in row1:
+            if row[1]>datetime.strftime(str(date), '%d-%b-%Y:%S-%M-%H'):
+                resp={
+                    "rideid":row[4],
+                    "usernane": row[0],
+                    "timestamp": row[1]
+                }
+                res.append(resp)
+        #print(row)
+        return jsonify(res),200
+        
 
-    return "This ride doesn't exist!"
+
+    if p==5:
+        cur = mysql.connection.cursor()
+        j=argnum
+        cur.execute("SELECT *  FROM rides where rideid='"+j+"'")
+        row = cur.fetchone()
+        #return jsonify(row)
+        if(row==None):
+            return 204
+        cur.execute("SELECT userz  FROM ride_users where rideid='"+j+"'")
+        res=[]
+        for row1 in cur:
+            res.append(row1[0])
+        #for x in row1:
+        #    res.append(x)
+
+        resp={
+            "rideid": row[4],
+            "created_by": row[0],
+            "timestamp": row[1],
+            "source": row[2],
+            "destination": row[3],
+            "users": res
+        }
+        result.append(resp)
+        cur.close()
+        print(resp)
+        #return resp, 200
+        return jsonify(resp),200
+
+    if p==6:
+        cur = mysql.connection.cursor()
+        j=argnum
+        cur.execute("SELECT *  FROM rides where rideid='"+j+"'")
+        row = cur.fetchone()
+        if(row==None):
+            return "Ride doesn't exist. Create a new ride now!",400
+
+        cur.execute("SELECT *  FROM users where username='"+request.json.get('username')+"'")
+        row1 = cur.fetchone()
+        if(row1==None):
+            return "User doesn't exist. Join today!", 400
+
+        cur.execute("SELECT *  FROM ride_users where userz='"+request.json.get('username')+"' AND rideid='"+j+"'")
+        row2 = cur.fetchone()
+        if(row2!=None):
+            return "This user is already in the ride.",400
+
+        return redirect(flask.url_for('writetodb'), code=307)
 
 
-
-
-app.run()
+    if p==7:
+        cur = mysql.connection.cursor()
+        j=argg
+        print(j)
+        cur.execute("SELECT *  FROM rides where rideid='"+j+"'")
+        row = cur.fetchone()
+        print(row)
+        if(row==None):
+            return "This ride doesn't exist. Can't delete.",400
+        else:
+            return redirect(flask.url_for('writetodb'), code=307)
+        
+    
+app.run(host='0.0.0.0', port=80)
