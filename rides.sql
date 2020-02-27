c3b699e2ac82321cafa500a8d785734318596632
Aarya Arun <45157845+aarya-arun@users.noreply.github.com>
2020-02-27 11:24:35 +0530

    Create rides.sql

---
+++ rides.sql
@@ -0,0 +1,25 @@
+CREATE TABLE rides(
+
+  created_by varchar(30),
+  timestamp varchar(40),
+  source int,
+  destination int,
+  rideId varchar(40)
+  
+  );
+  
+  CREATE TABLE rides_id(
+  
+    rideidstart int
+    
+    );
+    
+   
+   CREATE TABLE ride_users(
+   
+    rideid varchar(30),
+    userz varchar(30)
+    
+    );
+    
+    
